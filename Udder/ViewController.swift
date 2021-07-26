12e705d23eef2c833ace2762613a4043de4b2796
MohamedEl-mowafy <eng.mohamedelmwafy@gmail.com>
2021-07-26 21:56:39 +0200

    viewwillappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func viewDidLayoutSubviews() {
+        //
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
 
 }
 
